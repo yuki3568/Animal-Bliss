9eca2a0882b1dbd2c307e4d7843b2fb2dfe3c5ed
yuki3568 <iyoyuuki@MacBook-Air.local>
2025-03-02 02:18:53 +0900

    fix：user_mailer_test.rbを修正

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,12 +1,18 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
+  SUBJECT = "Reset password email"
+  FROM_EMAIL = "from@example.com"
+
+  def setup
+    @user = User.create(email: "to@example.org")
+  end
+
   test "reset_password_email" do
-    user = User.create(email: "to@example.org")
-    mail = UserMailer.reset_password_email(user)
-    assert_equal "Reset password email", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
+    mail = UserMailer.reset_password_email(@user)
+    assert_equal SUBJECT, mail.subject
+    assert_equal [ @user.email ], mail.to
+    assert_equal [ FROM_EMAIL ], mail.from
     assert_match "Hi", mail.body.encoded
   end
 end
